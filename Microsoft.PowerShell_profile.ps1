ebe6e8f943bb50f72f32807a8161bb65c6f5fb35
Nathaniel P <natepel13@gmail.com>
2024-05-25 01:52:44 -0400

    Finish removing personal changes

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -308,9 +308,3 @@ if (Get-Command zoxide -ErrorAction SilentlyContinue) {
         Write-Error "Failed to install zoxide. Error: $_"
     }
 }
-
-# PowerToys CommandNotFound module
-#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
-
-Import-Module -Name Microsoft.WinGet.CommandNotFound
-#f45873b3-b655-43a6-b217-97c00aa0db58
